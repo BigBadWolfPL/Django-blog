d6a3c7aee271d24d5a583067b64429ac58c537d2
BigBadWolfPL <robertbielickiwet@gmail.com>
2022-11-22 12:03:45 +0100

    added working buttons

--- blog/views.py
+++ blog/views.py
@@ -2,16 +2,15 @@ from django.shortcuts import render
 from urllib.request import urlopen
 import json
 
-url = "https://raw.githubusercontent.com/BigBadWolfPL/Blog-Flask/main/wpisy.json"
-
-with urlopen(url) as res:
-    data = json.load(res)
-
+posts = [
+    {"author": "Robert Bielicki", "title": "Nauka frameworka Django", "content": "Django is a high-level Python web framework that encourages rapid development and clean, pragmatic design. Built by experienced developers, it takes care of much of the hassle of web development, so you can focus on writing your app without needing to reinvent the wheel. It’s free and open source. ", "date_posted": "20.11.2022"}, 
+    {"author": "Natalia Marcińczyk", "title": "Dywan", "content": "Misia Natalia była bardzo zasmucona dywanem i trzba było go oddać :<", "date_posted": "22.11.2022"}
+]
 
 
 def home(request):
     context = {
-        'wpisy': data
+        'posts': posts
     }
     return render(request, 'blog/home.html', context)
 
