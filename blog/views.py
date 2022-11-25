9ffd86dbe88bbe17c08f1b0ba99b2fdfbd092b06
Robert Bielicki <robertbielickiwet@gmail.com>
2022-11-25 18:45:25 +0100

    Added: users image, forms for changing profile img, username, mail.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
@@ -29,15 +31,6 @@ def kontakt(request):
     }
     return render(request, 'blog/kontakt.html', context)
 
+@login_required
 def testowy(request):
-    
-    context = {
-        'title': 'Kontakt', 
-        'dane_kontaktowe': [
-            'GitHub: BigBadWolfPL', 
-            'LindedIn: Robert Bielicki', 
-            'Mail: robertbielickiwet@gmail.com', 
-            'Tel: 504-113-035'
-            ]
-    }
-    return render(request, 'blog/testowytem.html', context)
+    return render(request, 'blog/testowytem.html')
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
@@ -9,8 +9,8 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!. You are now able to log in')
+            username = form.cleaned_data.get('username') ### Opcjonalnie dodane
+            messages.success(request, f'Account created for {username}!. You are now able to log in') ### Opcjonalnie dodane
             return redirect('users-login')
     else:
         form = UserRegisterForm()
@@ -19,4 +19,22 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid and p_form.is_valid:
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Profile info updated!')
+        return redirect('users-profile')
+        
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'users/profile.html', context)
